e528f0efccd3f799a9fb22ec917be9cd3b9427a5
Ricardo Munda <rjmunda21@gmail.com>
2022-10-08 02:36:27 -0500

    .

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,16 @@
+let squares = document.getElementsByTagName("div");
 onload = function(){
-    let squares = document.getElementsByTagName("div")
     for (let i = 3; i < 12; i++) {
-    squares[i].setAttribute("class","square")
+    squares[i].setAttribute("class","square");
+    squares[i].onmouseover = function(){
+        squares[i].classList.add("class","hover")
     }
-};
+    squares[i].onmouseout = function(){
+        squares[i].classList.remove("class","hover")
+
+    }
+    };
+}
+
+
 
